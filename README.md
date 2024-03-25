361db3f599a1d7e408e14a11d4846c149d024ab5
etim <etimpaul22@gmail.com>
2024-03-25 11:06:38 +0100

    added frontend coding

--- README.md
+++ README.md
@@ -10,12 +10,11 @@ You will start with the backend setup locally and run it in a no-backend environ
 2. Install the dependencies - "npm install"
 3. Start your backend docker container - "npm run backend"
 4. Start your backend - "npm run start"
-5. Deploy the backend locally for testing with this command and get a deployed address - "sunodo send generic"
 
-With your backend docker running and the project running perfectly. You can start the frontend
+With your backend docker running and the project backend running locally. You can start the frontend
 
 1. CD into the frontend directory - "cd /frontend"
 2. don't worry, the dependencies were install when you bootstrap the project
 3. run "npm run start" to start the frontend
 
-To fully test, make sure, you are connected to your metamask wallet and to the local chain.
+To fully test, make sure, you are connected to your metamask wallet and to the local chain. (RPC-  http://127.0.0.1:8545/, chain-id - 31337)
--- backend/dapp.js
+++ backend/dapp.js
@@ -10,9 +10,9 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-const { ethers } = require("ethers");
+const ethers = require("ethers");
 const { Wallet } = require("cartesi-wallet");
-const { stringToHex } = "viem";
+const { stringToHex } = require("viem");
 let wallet = new Wallet(new Map());
 
 const rollup_server = process.env.ROLLUP_HTTP_SERVER_URL;
@@ -21,20 +21,21 @@ const DAPP_ADDRESS_REALY = "0xF5DE34d6BbC0446E2a45719E718efEbaaE179daE";
 
 async function handle_advance(data) {
     console.log("Received advance request data " + JSON.stringify(data));
-    let input = data.payload;
-    let str = Buffer.from(input.substr(2), "hex").toString("utf8");
-    let json = JSON.parse(str);
+    const payload = data.payload;
     let JSONpayload = {};
     try {
         if (
             String(data.metadata.msg_sender).toLowerCase() ===
             DAPP_ADDRESS_REALY.toLowerCase()
         ) {
-            console.log("setting Dapp address:", input);
+            console.log("setting Dapp address:", payload);
             DAPP_ADDRESS = payload;
         }
 
-        console.log("payload:" + JSON.stringify(input));
+        console.log("payload:" + JSON.stringify(payload));
+        const payloadStr = ethers.toUtf8String(payload);
+        JSONpayload = JSON.parse(payloadStr);
+        console.log(`received request ${JSON.stringify(JSONpayload)}`);
     } catch (e) {
         console.log("error is:", e);
         console.log(`Adding notice with binary value "${payload}"`);
@@ -48,61 +49,74 @@ async function handle_advance(data) {
         return "reject";
     }
 
-    let notice;
+    let advance_req;
     try {
-        if (json.method == "ether_deposit") {
-            notice = wallet.ether_deposit_process(json.amount);
-            await fetch(rollup_server + "/notice", {
+        if (JSONpayload.method == "ether_deposit") {
+            let etherDep = wallet.ether_deposit_process(JSONpayload.amount.toString());
+            console.log("deposit ether....");
+            const hexresult = stringToHex(etherDep);
+            console.log("The result is :", hexresult);
+
+            advance_req = await fetch(rollup_server + "/notice", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ payload: notice.payload }),
+                body: JSON.stringify({ payload: hexresult }),
             });
-            const json = await notice.json();
-            console.log("Received notice status " + notice.status + " with body " + JSON.stringify(json));
-            return "accept";
         } else if (json.method == "ether_withdraw") {
-            notice = wallet.ether_withdraw(json.dapp, data.metadata.msg_sender, BigInt(json.amount));
-            await fetch(rollup_server + "/notice", {
+            let etherWith = wallet.ether_withdraw(JSONpayload.dapp, data.metadata.msg_sender, BigInt(JSONpayload.amount));
+            console.log("withdrawing ether....");
+            const hexresult = stringToHex(etherWith);
+            console.log("The result is :", hexresult);
+
+            advance_req = await fetch(rollup_server + "/notice", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ payload: notice.payload }),
+                body: JSON.stringify({ payload: hexresult }),
             });
-            const json = await notice.json();
-            console.log("Received notice status " + notice.status + " with body " + JSON.stringify(json));
-            return "accept";
         } else if (json.method == "ether_transfer") {
-            notice = wallet.ether_transfer(data.metadata.msg_sender, json.to, BigInt(data.json.amount));
-            await fetch(rollup_server + "/notice", {
+            let etherTrf = wallet.ether_transfer(data.metadata.msg_sender, JSONpayload.to, BigInt(JSONpayload.amount));
+            console.log("transfering ether....");
+            const hexresult = stringToHex(etherTrf);
+            console.log("The result is :", hexresult);
+
+            advance_req = await fetch(rollup_server + "/notice", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ payload: notice.payload }),
+                body: JSON.stringify({ payload: hexresult }),
             });
-            const json = await notice.json();
-            console.log("Received notice status " + notice.status + " with body " + JSON.stringify(json));
-            return "accept";
-        } else if (json.method == "ether_deposit") {
-            notice = wallet.ether_deposit_process(BigInt(json.amount));
-            await fetch(rollup_server + "/notice", {
+        } else if (json.method == "erc_deposit") {
+            let ercDep = wallet.erc20_deposit_process(BigInt(JSONpayload.amount));
+            console.log("depositing erc20....");
+            const hexresult = stringToHex(ercDep);
+            console.log("The result is :", hexresult);
+
+            advance_req = await fetch(rollup_server + "/notice", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ payload: notice.payload }),
+                body: JSON.stringify({ payload: hexresult }),
             });
-            const json = await notice.json();
-            console.log("Received notice status " + notice.status + " with body " + JSON.stringify(json));
-            return "accept";
+
         } else if (json.method == "erc_transfer") {
-            notice = wallet.erc20_transfer(json.from, json.to, json.erc20, BigInt(json.amount));
-            await fetch(rollup_server + "/notice", {
+            let ercTrf = wallet.erc20_transfer(JSONpayload.from, JSONpayload.to, JSONpayload.erc20, BigInt(JSONpayload.amount));
+            console.log("transfering erc20....");
+            const hexresult = stringToHex(ercTrf);
+            console.log("The result is :", hexresult);
+
+            advance_req = await fetch(rollup_server + "/notice", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ payload: notice.payload }),
+                body: JSON.stringify({ payload: hexresult }),
             });
         } else if (json.method == "erc_withdraw") {
-            voucher = wallet.erc20_withdraw(json.from, json.erc20, BigInt(json.amount));
-            await fetch(rollup_server + "/voucher", {
+            let ercWith = wallet.erc20_withdraw(JSONpayload.from, JSONpayload.erc20, BigInt(JSONpayload.amount));
+            console.log("withdrawing erc20....");
+            const hexresult = stringToHex(ercWith);
+            console.log("The result is :", hexresult);
+
+            advance_req = await fetch(rollup_server + "/voucher", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ payload: voucher.payload, destination: voucher.destination }),
+                body: JSON.stringify({ payload: hexresult }),
             });
         }
     }
@@ -119,6 +133,14 @@ async function handle_advance(data) {
         });
         return "reject";
     }
+    const json = await advance_req?.json();
+    console.log(
+        "Received  status " +
+        advance_req?.status +
+        " with body " +
+        JSON.stringify(json)
+    );
+    return "accept";
 }
 
 async function handle_inspect(data) {
